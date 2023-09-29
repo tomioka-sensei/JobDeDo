05178dc0bef296953a8ed6fd9da050e099f0cd1a
parteektiwari <tiwariprateek1976@gmail.com>
2023-09-29 23:15:14 +0530

    minor

--- src/Components/Position.jsx
+++ src/Components/Position.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 const Position = () => {
   return (
     <div>
-        <h1 className='text-3xl font-semibold mx-auto justify-center flex'>Role By Top Rated Comapanies</h1>
+        <h1 className='text-3xl font-semibold mx-auto justify-center flex '>Role By Top Rated Comapanies</h1>
 
         {/* Card component */}
 
-        <div className='w-[200px] border-lime-700'>
-            <h2>Accountant<span>Sih-1333</span></h2>
-            <h3><span>Location</span> Gurugram,Haryana</h3>
-            <h3><span>PostedBy:</span>CTO-Polygon</h3>
-
+        <div className='w-[300px] border border-black bg-[#e9d5ff] p-5 rounded-lg mt-10 mx-0 h-[150px] '>
+            <h2 className='text-2xl text-[#5b21b6] font-semibold'>Accountant<span>(KPMG)</span></h2>
+            <h3 className='text-base font-light'><span className='font-medium'>Location</span> Gurugram,Haryana</h3>
+            <h3 className='text-base font-light'><span className='font-medium'>PostedBy:</span> Tech-Lead</h3>
+            <div className='flex my-5 mx-16'>
+              <button className='text-white bg-[#4c1d95] w-24'>View Details</button>
+            </div>
+ 
 
         </div>
 
